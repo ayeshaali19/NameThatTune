7aa82d61db399155a73b9e5d510d394f377dddd2
Ali <ayesha.ali19@mbpr1315-1786.trinity.nyc.ny.us>
2017-11-30 10:11:38 -0500

    message

--- MusicLibrary.java
+++ MusicLibrary.java
@@ -1,31 +1,3 @@
-/******************************************************************************
- *  Compilation:  javac PlayThatTune.java
- *  Execution:    java PlayThatTune < input.txt
- *  Dependencies: StdAudio.java StdAudio.java
- *
- *  This is a data-driven program that plays pure tones from
- *  the notes on the chromatic scale, specified on standard input
- *  by their distance from concert A.
- *
- *  % java PlayThatTune < elise.txt
- *
- *
- *  Data files
- *  ----------
- *  http://www.cs.princeton.edu/introcs/21function/elise.txt
- *  http://www.cs.princeton.edu/introcs/21function/freebird.txt
- *  http://www.cs.princeton.edu/introcs/21function/Ascale.txt
- *  http://www.cs.princeton.edu/introcs/21function/National_Anthem.txt
- *  http://www.cs.princeton.edu/introcs/21function/looney.txt
- *  http://www.cs.princeton.edu/introcs/21function/StairwayToHeaven.txt
- *  http://www.cs.princeton.edu/introcs/21function/entertainer.txt
- *  http://www.cs.princeton.edu/introcs/21function/old-nassau.txt
- *  http://www.cs.princeton.edu/introcs/21function/arabesque.txt
- *  http://www.cs.princeton.edu/introcs/21function/firstcut.txt 
- *  http://www.cs.princeton.edu/introcs/21function/tomsdiner.txt
- *
- ******************************************************************************/
-
 public class MusicLibrary {
 
     public static void main(String[] args) {
